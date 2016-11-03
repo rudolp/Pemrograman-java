96f6c2d9add5ca5f1098f8224d54bc6d84c11d07
rudolp <setyawanrudil20@gmail.com>
2016-11-03 21:48:57 +0700

    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga 4 baris kedua masih sama hanya untuk pemanasan saja 3 baris selanjutnya, saya mengawali langkah untuk mengkondisikan karakter yang akan saya pilih menjadi bentuk segitiga siku 4 baris kode lagi saya gunakan untuk memberikan jarak antar karakter agar tidk terlalu berhimpitan/biar rapi 5 baris selanjutnya adalah lanjutan kode untuk membuat segitiga dengan karakter yang saya pilih adalah* (asterik) Sampai proses ini, segitiga sudah bisa dioutput, cuma langkah finisingnya belum bagus Baris selanjutnya saya akan mencoba untuk memperbaiki finishing setelah program dijalankan, Pada 4 bris ini saya membuat pilihan n untuk mereturn program dan baris terakhir untuk mengakhiri program secara smoth
    Class kedua Segitiga sama kaki
    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga
    4 baris kedua masih sama hanya untuk pemanasan saja
    3 baris selanjutnya, saya mengawali langkah untuk mengkondisikan karakter yang akan saya pilih menjadi bentuk segitiga samakaki
    4 baris kode lagi saya gunakan untuk memberikan jarak antar karakter agar tidk terlalu berhimpitan/biar rap
    5 baris selanjutnya adalah lanjutan kode untuk membuat segitiga dengan karakter yang saya pilih adalah* (asteriki)
    5 baris seanjutnya adalah kode untuk melengkapi kode kode diatas, sanpai pda tahap ini, segitiga samakaki sudah bisa dibentuk, tapi finishing belum sempurna
    kemudian code terakhir saya buat untuk menutup program java agar lebih smoth, kode itu saya copikan dari program segitiga_siku_kanan_atas yang kebetulan formatnya sama.
    
    
    Class ketiga Segitiga siku kiri bawahi
    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga

---
+++ src/tugas1/Segitiga_siku_kiri_bawah.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author windows10
+ */
+public class Segitiga_siku_kiri_bawah {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int alas;
+        
+        boolean menu =true;
+        while(menu)
+    }
+}
