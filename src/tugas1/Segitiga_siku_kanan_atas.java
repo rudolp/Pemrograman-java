328a6ca0ed0f4c0e2b6da9430ed4f1a33ac076ef
rudolp <setyawanrudil20@gmail.com>
2016-11-03 22:51:23 +0700

    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga 4 baris kedua masih sama hanya untuk pemanasan saja 3 baris selanjutnya, saya mengawali langkah untuk mengkondisikan karakter yang akan saya pilih menjadi bentuk segitiga siku 4 baris kode lagi saya gunakan untuk memberikan jarak antar karakter agar tidk terlalu berhimpitan/biar rapi 5 baris selanjutnya adalah lanjutan kode untuk membuat segitiga dengan karakter yang saya pilih adalah* (asterik) Sampai proses ini, segitiga sudah bisa dioutput, cuma langkah finisingnya belum bagus Baris selanjutnya saya akan mencoba untuk memperbaiki finishing setelah program dijalankan, Pada 4 bris ini saya membuat pilihan n untuk mereturn program dan baris terakhir untuk mengakhiri program secara smoth
    Class kedua Segitiga sama kaki
    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga
    4 baris kedua masih sama hanya untuk pemanasan saja
    3 baris selanjutnya, saya mengawali langkah untuk mengkondisikan karakter yang akan saya pilih menjadi bentuk segitiga samakaki
    4 baris kode lagi saya gunakan untuk memberikan jarak antar karakter agar tidk terlalu berhimpitan/biar rap
    5 baris selanjutnya adalah lanjutan kode untuk membuat segitiga dengan karakter yang saya pilih adalah* (asteriki)
    5 baris seanjutnya adalah kode untuk melengkapi kode kode diatas, sanpai pda tahap ini, segitiga samakaki sudah bisa dibentuk, tapi finishing belum sempurna
    kemudian code terakhir saya buat untuk menutup program java agar lebih smoth, kode itu saya copikan dari program segitiga_siku_kanan_atas yang kebetulan formatnya sama.
    
    
    Class ketiga Segitiga siku kiri bawahi
    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga
    baris selanjutnya masih berhubungan dengan beberapa baris awal yang telah saya sebutkan tadi,
    4 baris selanjutnya, saya mengawali langkah untuk mengkondisikan karakter yang akan saya pilih menjadi bentuk segitiga siku kiri bawah
    4 baris baru saya buat sebagai kode utama untuk membuat segitiga
    3 baris kode selanjutnya untuk menampilkan output segitiga, sampai pada kode ini, kita sudah bisa menampilkan segitiga siku kiri bawah
    Sebagai penutup, kode selanjutnya adalah untuk merapikan program agar jika sudah selesai bisa ditutp atau diulang lagi dengan 1 langkah mudah, kode tersebut hanya saya copykan dari program segitiga_siku_kanak_bawah.java yang telah saya buat tadi.
    
    class keempat segitiga siku kiri atas
    pertama saya membuat scrip untuk memungkinkan kita menginput dari keyboard dengan memanggil library,
    Baris selanjutnya untk melengkapi script baris sebelumnya yaitu agar kita bisa melakukan input dari keyboard
    3 baris selanjutnya adalah untuk memulai kode untuk membuat segitiga,
    5 bari setelahnya adalah kode untuk menyelesaikan segitiga, sampai pada kode ini saya sudah bisa menampilkan segitiga siku kiri atas
    Sebagai penutup, kode selanjutnya adalah untuk merapikan program agar jika sudah selesai bisa ditutp atau diulang lagi dengan 1 langkah mudah, kode tersebut hanya saya copykan dari program segitiga_siku_kanak_bawah.java yang telah saya buat tadi.
    
    class kelima segitiga siku kiri atas
    pertama saya membuat scrip untuk memungkinkan kita menginput dari keyboard dengan memanggil library,
    Baris selanjutnya untk melengkapi script baris sebelumnya yaitu agar kita bisa melakukan input dari keyboard
    3 baris selanjutnya adalah untuk memulai kode untuk membuat segitiga,
    5 bari setelahnya adalah kode untuk menyelesaikan segitiga, sampai pada kode ini saya sudah bisa menampilkan segitiga siku kanan atas

--- src/tugas1/Segitiga_siku_kanan_atas.java
+++ src/tugas1/Segitiga_siku_kanan_atas.java
@@ -22,14 +22,22 @@ public class Segitiga_siku_kanan_atas {
             System.out.print("Masukkan jumlah alas = ");
         	alas = scan.nextInt();
         
-        	System.out.println("\nCetak Segitiga Siku kiri atas");
+        	System.out.println("\nCetak Segitiga Siku kanan atas");
         	//Untuk memudahkan User interface saja
                 
         for (int i=alas;i<=1;i--)
         	{
+                    for (int x=i;x<alas;x++)
+                    {
+                        System.out.print(" ");
+                    }
+                    for (int k=0;k<i;k++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
                 }
         }
-                }
+                
     }
     
 }
