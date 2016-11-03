d1ebe867c2cc934ca5b157274cb385fa0f0dfeeb
rudolp <setyawanrudil20@gmail.com>
2016-11-03 21:16:05 +0700

    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga 4 baris kedua masih sama hanya untuk pemanasan saja 3 baris selanjutnya, saya mengawali langkah untuk mengkondisikan karakter yang akan saya pilih menjadi bentuk segitiga siku 4 baris kode lagi saya gunakan untuk memberikan jarak antar karakter agar tidk terlalu berhimpitan/biar rapi 5 baris selanjutnya adalah lanjutan kode untuk membuat segitiga dengan karakter yang saya pilih adalah* (asterik) Sampai proses ini, segitiga sudah bisa dioutput, cuma langkah finisingnya belum bagus Baris selanjutnya saya akan mencoba untuk memperbaiki finishing setelah program dijalankan, Pada 4 bris ini saya membuat pilihan n untuk mereturn program dan baris terakhir untuk mengakhiri program secara smoth
    Class kedua Segitiga sama kaki
    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga

---
+++ src/tugas1/Segitiga_sama_kaki.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author windows10
+ */
+public class Segitiga_sama_kaki {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int alas;
+        
+        boolean menu =true;
+        while(menu)
+        
+    }
+    
+}
