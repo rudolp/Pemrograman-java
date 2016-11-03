be9c74f4e379622dc43511ce636301e6ed8f8fcd
rudolp <setyawanrudil20@gmail.com>
2016-11-03 20:36:49 +0700

    Pertama saya akan buat kode untuk menginput panjang alas yang aka saya gunakan untuk membangun segitiga

---
+++ src/tugas1/Segitiga_siku_kanan_atas.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author windows10
+ */
+public class Segitiga_siku_kanan_atas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        int alas;
+        
+        boolean menu =true;
+        while(menu)
+        {
+            
+        }
+        }
+    }
+    
